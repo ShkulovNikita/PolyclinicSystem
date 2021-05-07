96d9fa594a34595700da3003f86405932f29f9de
NikitaShkulov <tyuiop_2@mail.ru>
2021-05-07 23:09:30 +0700

    Реализован профиль пациента.

--- PolyclinicSystem/Forms/Profiles/PatientForm.Designer.cs
+++ PolyclinicSystem/Forms/Profiles/PatientForm.Designer.cs
@@ -30,27 +30,163 @@ namespace PolyclinicSystem.Forms
         private void InitializeComponent()
         {
             this.visitsButton = new System.Windows.Forms.Button();
+            this.doctorsButton = new System.Windows.Forms.Button();
+            this.editProfileButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.jobPlaceLabel = new System.Windows.Forms.Label();
+            this.phoneLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.birthdateLabel = new System.Windows.Forms.Label();
+            this.omsLabel = new System.Windows.Forms.Label();
+            this.genderLabel = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // visitsButton
             // 
-            this.visitsButton.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.visitsButton.Location = new System.Drawing.Point(506, 180);
+            this.visitsButton.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.visitsButton.Location = new System.Drawing.Point(473, 84);
             this.visitsButton.Name = "visitsButton";
-            this.visitsButton.Size = new System.Drawing.Size(75, 23);
+            this.visitsButton.Size = new System.Drawing.Size(150, 50);
             this.visitsButton.TabIndex = 0;
-            this.visitsButton.Text = "button1";
+            this.visitsButton.Text = "Приемы у врачей";
             this.visitsButton.UseVisualStyleBackColor = true;
             this.visitsButton.Click += new System.EventHandler(this.visitsButton_Click);
+            // 
+            // doctorsButton
+            // 
+            this.doctorsButton.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.doctorsButton.Location = new System.Drawing.Point(473, 150);
+            this.doctorsButton.Name = "doctorsButton";
+            this.doctorsButton.Size = new System.Drawing.Size(150, 50);
+            this.doctorsButton.TabIndex = 1;
+            this.doctorsButton.Text = "Врачи";
+            this.doctorsButton.UseVisualStyleBackColor = true;
+            // 
+            // editProfileButton
+            // 
+            this.editProfileButton.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.editProfileButton.Location = new System.Drawing.Point(473, 219);
+            this.editProfileButton.Name = "editProfileButton";
+            this.editProfileButton.Size = new System.Drawing.Size(150, 50);
+            this.editProfileButton.TabIndex = 2;
+            this.editProfileButton.Text = "Редактировать профиль";
+            this.editProfileButton.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.jobPlaceLabel);
+            this.panel1.Controls.Add(this.phoneLabel);
+            this.panel1.Controls.Add(this.nameLabel);
+            this.panel1.Controls.Add(this.addressLabel);
+            this.panel1.Controls.Add(this.birthdateLabel);
+            this.panel1.Controls.Add(this.omsLabel);
+            this.panel1.Controls.Add(this.genderLabel);
+            this.panel1.Location = new System.Drawing.Point(19, 84);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(421, 337);
+            this.panel1.TabIndex = 13;
+            // 
+            // jobPlaceLabel
+            // 
+            this.jobPlaceLabel.AutoSize = true;
+            this.jobPlaceLabel.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.jobPlaceLabel.Location = new System.Drawing.Point(17, 210);
+            this.jobPlaceLabel.Name = "jobPlaceLabel";
+            this.jobPlaceLabel.Size = new System.Drawing.Size(123, 19);
+            this.jobPlaceLabel.TabIndex = 9;
+            this.jobPlaceLabel.Text = "Место работы:";
+            // 
+            // phoneLabel
+            // 
+            this.phoneLabel.AutoSize = true;
+            this.phoneLabel.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.phoneLabel.Location = new System.Drawing.Point(17, 171);
+            this.phoneLabel.Name = "phoneLabel";
+            this.phoneLabel.Size = new System.Drawing.Size(80, 19);
+            this.phoneLabel.TabIndex = 8;
+            this.phoneLabel.Text = "Телефон:";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nameLabel.Location = new System.Drawing.Point(17, 19);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(56, 19);
+            this.nameLabel.TabIndex = 7;
+            this.nameLabel.Text = "ФИО: ";
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.addressLabel.Location = new System.Drawing.Point(17, 246);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(62, 19);
+            this.addressLabel.TabIndex = 3;
+            this.addressLabel.Text = "Адрес:";
+            // 
+            // birthdateLabel
+            // 
+            this.birthdateLabel.AutoSize = true;
+            this.birthdateLabel.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.birthdateLabel.Location = new System.Drawing.Point(17, 134);
+            this.birthdateLabel.Name = "birthdateLabel";
+            this.birthdateLabel.Size = new System.Drawing.Size(132, 19);
+            this.birthdateLabel.TabIndex = 2;
+            this.birthdateLabel.Text = "Дата рождения:";
+            // 
+            // omsLabel
+            // 
+            this.omsLabel.AutoSize = true;
+            this.omsLabel.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.omsLabel.Location = new System.Drawing.Point(17, 96);
+            this.omsLabel.Name = "omsLabel";
+            this.omsLabel.Size = new System.Drawing.Size(51, 19);
+            this.omsLabel.TabIndex = 1;
+            this.omsLabel.Text = "ОМС:";
+            // 
+            // genderLabel
+            // 
+            this.genderLabel.AutoSize = true;
+            this.genderLabel.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.genderLabel.Location = new System.Drawing.Point(17, 58);
+            this.genderLabel.Name = "genderLabel";
+            this.genderLabel.Size = new System.Drawing.Size(44, 19);
+            this.genderLabel.TabIndex = 0;
+            this.genderLabel.Text = "Пол:";
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(-1, -1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(666, 57);
+            this.panel2.TabIndex = 14;
             // 
             // PatientForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(648, 444);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.editProfileButton);
+            this.Controls.Add(this.doctorsButton);
             this.Controls.Add(this.visitsButton);
             this.Name = "PatientForm";
-            this.Text = "PatientForm";
+            this.Text = "Профиль пациента";
+            this.Load += new System.EventHandler(this.PatientForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -58,5 +194,16 @@ namespace PolyclinicSystem.Forms
         #endregion
 
         private System.Windows.Forms.Button visitsButton;
+        private System.Windows.Forms.Button doctorsButton;
+        private System.Windows.Forms.Button editProfileButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label addressLabel;
+        private System.Windows.Forms.Label birthdateLabel;
+        private System.Windows.Forms.Label omsLabel;
+        private System.Windows.Forms.Label genderLabel;
+        private System.Windows.Forms.Label phoneLabel;
+        private System.Windows.Forms.Label jobPlaceLabel;
+        private System.Windows.Forms.Panel panel2;
     }
 }
--- PolyclinicSystem/Forms/Profiles/PatientForm.cs
+++ PolyclinicSystem/Forms/Profiles/PatientForm.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using PolyclinicSystem.Forms.Functions;
 
@@ -22,6 +15,36 @@ namespace PolyclinicSystem.Forms
         {
             VisitsListForm visitsForm = new VisitsListForm();
             visitsForm.Show();
+        }
+
+        private void InitializeLabels()
+        {
+            try
+            {
+                nameLabel.Text = "ФИО: " + MainForm.CurPatient.User.Name;
+                omsLabel.Text = "ОМС: " + MainForm.CurPatient.OMS;
+                phoneLabel.Text = "Телефон: " + MainForm.CurPatient.Phone;
+                jobPlaceLabel.Text = "Место работы/учебы: " + MainForm.CurPatient.JobPlace;
+                addressLabel.Text = "Адрес:\n" + MainForm.CurPatient.Address;
+                birthdateLabel.Text = "Дата рождения: " + MainForm.CurPatient.BirthDate;
+
+                if (MainForm.CurPatient.Gender == "м")
+                    genderLabel.Text = "Пол: мужской";
+                else
+                    genderLabel.Text = "Пол: женский";
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLog(ex);
+                ErrorHandler.ShowError("Не удалось получить информацию о пациенте");
+                Close();
+            }
+        }
+
+        private void PatientForm_Load(object sender, EventArgs e)
+        {
+            //загрузить информацию о пользователе на форму
+            InitializeLabels();
         }
     }
 }
